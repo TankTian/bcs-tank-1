a64ce5c15c24cec695c9e38a6c492bf73767888e
paas <paas@cse-codegen-backend-1310641886-skl2b>
2018-10-12 03:59:54 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/cse/controller/Bcstank1Delegate.java
@@ -0,0 +1,14 @@
+package com.service.cse.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Bcstank1Delegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
